6a9f04ff0214c5e855b55f27cc682909b4b7b266
Heejong Ahn <heejongahn@gmail.com>
2019-05-19 17:48:41 +0900

    Implement BottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,51 @@
 import 'package:flutter/material.dart';
-import 'screens/book_list/index.dart';
 
-void main() => runApp(MyApp());
+import 'package:booklog/models/book.dart';
+import 'package:booklog/screens/add_book/index.dart';
+import 'package:booklog/screens/book_list/index.dart';
 
-class MyApp extends StatelessWidget {
+void main() => runApp(MaterialApp(home: MyApp()));
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _pageIndex = 0;
+
+  final homeItem = const BottomNavigationBarItem(
+    icon: Icon(Icons.list),
+    title: Text('피드'),
+  );
+
+  final addItem = const BottomNavigationBarItem(
+      icon: Icon(Icons.create), title: Text('새 리뷰'));
+
+  final myItem = const BottomNavigationBarItem(
+      icon: Icon(Icons.person), title: Text('MY'));
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(),
-      home: Books(),
+    return Scaffold(
+      body: IndexedStack(index: _pageIndex, children: [
+        Books(),
+        AddBook((Book book) => {}),
+        AddBook((Book book) => {}),
+      ]),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          homeItem,
+          addItem,
+          myItem,
+        ],
+        currentIndex: _pageIndex,
+        onTap: (int index) {
+          setState(() {
+            _pageIndex = index;
+          });
+        },
+      ),
     );
   }
 }
