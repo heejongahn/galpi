8a586e5e889869cfbbf1ac28df09eeabef35d8f5
Heejong Ahn <heejongahn@gmail.com>
2019-05-20 00:39:25 +0900

    Implement Review model and DB helper

---
+++ lib/models/review.dart
@@ -0,0 +1,81 @@
+// import 'package:booklog/models/book.dart';
+
+enum ReadingStatus {
+  hasntStarted,
+  reading,
+  finishedReading,
+}
+
+class Review {
+  final int id;
+  final int stars;
+  final String title;
+  final String body;
+  final DateTime readingStartedAt;
+  final DateTime readingFinishedAt;
+  final DateTime createdAt;
+  final DateTime lastModifiedAt;
+
+  static final table = 'Review';
+  static final columnId = 'id';
+  static final columnStars = 'stars';
+  static final columnTitle = 'title';
+  static final columnBody = 'body';
+  static final columnReadingStartedAt = 'readingStartedAt';
+  static final columnReadingFinishedAt = 'readingFinishedAt';
+  static final columnCreatedAt = 'createdAt';
+  static final columnLastModifiedAt = 'lastModifiedAt';
+
+  Review(
+      {this.id,
+      this.stars,
+      this.title,
+      this.body,
+      this.readingStartedAt,
+      this.readingFinishedAt,
+      this.createdAt,
+      this.lastModifiedAt});
+
+  static fromMap(Map<String, dynamic> map) {
+    return Review(
+      id: map[columnId],
+      stars: map[columnStars],
+      title: map[columnTitle],
+      body: map[columnBody],
+      readingStartedAt: map[columnReadingStartedAt],
+      readingFinishedAt: map[columnReadingFinishedAt],
+      createdAt: map[columnCreatedAt],
+      lastModifiedAt: map[columnLastModifiedAt],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    var map = <String, dynamic>{
+      columnStars: stars,
+      columnTitle: title,
+      columnBody: body,
+      columnReadingStartedAt: readingStartedAt,
+      columnReadingFinishedAt: readingFinishedAt,
+      columnCreatedAt: createdAt,
+      columnLastModifiedAt: lastModifiedAt,
+    };
+
+    if (id != null) {
+      map[columnId] = id;
+    }
+
+    return map;
+  }
+
+  ReadingStatus get readingStatus {
+    if (readingStartedAt == null) {
+      return ReadingStatus.hasntStarted;
+    }
+
+    if (readingFinishedAt == null) {
+      return ReadingStatus.reading;
+    }
+
+    return ReadingStatus.finishedReading;
+  }
+}
---
+++ lib/utils/database_helpers.dart
@@ -0,0 +1,74 @@
+import 'dart:io';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:booklog/models/review.dart';
+
+class DatabaseHelper {
+  static final _databaseName = "MyDatabase.db";
+  static final _databaseVersion = 1;
+
+  DatabaseHelper._privateConstructor();
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+
+  static Database _database;
+  Future<Database> get database async {
+    if (_database != null) {
+      return _database;
+    }
+
+    _database = await _initDatabase();
+    return _database;
+  }
+
+  // open the database
+  _initDatabase() async {
+    // The path_provider plugin gets the right directory for Android or iOS.
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, _databaseName);
+    // Open the database. Can also add an onUpdate callback parameter.
+    return await openDatabase(path,
+        version: _databaseVersion, onCreate: _onCreate);
+  }
+
+  // SQL string to create the database
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+              CREATE TABLE ${Review.table} (
+                ${Review.columnId} INTEGER PRIMARY KEY,
+                ${Review.columnStars} INTEGER,
+                ${Review.columnTitle} TEXT NOT NULL,
+                ${Review.columnBody} TEXT NOT NULL,
+                ${Review.columnReadingStartedAt} DATETIME,
+                ${Review.columnReadingFinishedAt} DATETIME,
+                ${Review.columnCreatedAt} DATETIME DEFAULT CURRENT_TIMESTAMP,
+                ${Review.columnLastModifiedAt} DATETIME DEFAULT CURRENT_TIMESTAMP
+              );
+              ''');
+  }
+
+  Future<int> insertReview(Review review) async {
+    Database db = await database;
+    int id = await db.insert(Review.table, review.toMap());
+    return id;
+  }
+
+  Future<List<Review>> queryAllReviews() async {
+    Database db = await database;
+    List<Map> maps = await db.query(Review.table);
+
+    return maps.map((map) => Review.fromMap(map)).cast<Review>().toList();
+  }
+
+  Future<Review> queryReview(int id) async {
+    Database db = await database;
+    List<Map> maps = await db
+        .query(Review.table, where: '${Review.columnId} = ?', whereArgs: [id]);
+
+    if (maps.length > 0) {
+      return Review.fromMap(maps.first);
+    }
+
+    return null;
+  }
+}
