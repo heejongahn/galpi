47e883a18ef034a8e2071820d35f42408c2de907
Heejong Ahn <heejongahn@gmail.com>
2019-07-27 13:12:36 +0900

    Save version informatiton to shared preference

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,10 @@ import 'package:galpi/components/screen_with_navigator/index.dart';
 import 'package:galpi/screens/book_list/index.dart';
 import 'package:galpi/screens/review_list/index.dart';
 import 'package:galpi/utils/theme.dart';
+import 'package:package_info/package_info.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+const SHARED_PREFERENCE_VERSION_KEY = 'version';
 final primaryColor = Color.fromRGBO(0xff, 0x74, 0x73, 1);
 
 void main() => runApp(MaterialApp(
@@ -38,6 +41,21 @@ class _MyAppState extends State<MyApp> {
 
   final myItem = const BottomNavigationBarItem(
       icon: Icon(Icons.person), title: Text('내 독후감'));
+
+  @override
+  void initState() {
+    super.initState();
+    PackageInfo.fromPlatform().then((info) async {
+      final prefs = await SharedPreferences.getInstance();
+      final versionFromPrefs = prefs.getString(SHARED_PREFERENCE_VERSION_KEY);
+      final lastVersion = versionFromPrefs != null ? versionFromPrefs : '1.0.0';
+
+      if (lastVersion != info.version) {
+        // TODO: 버전 업데이트 로직
+      }
+      prefs.setString(SHARED_PREFERENCE_VERSION_KEY, info.version);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
