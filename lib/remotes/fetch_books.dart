bac7ed6d1291dfb285eb4982361f55bc496fa65f
Heejong Ahn <heejongahn@gmail.com>
2019-08-20 12:35:00 +0900

    Prevent empty query for book search

--- lib/remotes/fetch_books.dart
+++ lib/remotes/fetch_books.dart
@@ -10,6 +10,10 @@ const API_ENDPOINT = "https://dapi.kakao.com/v3/search/book";
 Future<List<Book>> fetchBooks({String query}) async {
   Secret secret = await SecretLoader(secretPath: "secrets/keys.json").load();
 
+  if (query == '') {
+    return Future.value([]);
+  }
+
   final response = await http.get('${API_ENDPOINT}?query=${query}',
       headers: {'Authorization': 'KakaoAK ${secret.kakaoRestApiKey}'});
 
