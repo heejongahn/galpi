b2698643e8d8f3e47ed0a838352e2375f0738926
Heejong Ahn <heejongahn@gmail.com>
2020-01-27 20:46:19 +0900

    Implement bulk uploading review

--- lib/migrations/v1_0_4_upload_to_server.dart
+++ lib/migrations/v1_0_4_upload_to_server.dart
@@ -1,17 +1,21 @@
 import 'package:galpi/remotes/create_book.dart';
+import 'package:galpi/remotes/review/create.dart';
 import 'package:galpi/utils/database_helpers.dart';
 
 Future<void> v1_0_4_uploadToServer() async {
   final List<Future> futures = [];
   final queryResult = await DatabaseHelper.instance.queryAllReviews();
 
-  final books = queryResult.item2;
+  final size = queryResult.item1.length;
 
-  // TODO: Upload all reviews
-  // final reviews = queryResult.item1;
-  // futures.addAll(reviews.map((review) => {}));
+  for (var i = 0; i < size; i++) {
+    final review = queryResult.item1[i];
+    final book = queryResult.item2[i];
 
-  futures.addAll(books.map((book) => createBook(book: book)));
+    futures.add(createBook(book: book).then((bookId) {
+      createReview(review: review, bookId: bookId);
+    }));
+  }
 
   return Future.wait(futures);
 }
