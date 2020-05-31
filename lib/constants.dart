a1347a2c5cac557b80eef36182185736dce5eb13
Heejong Ahn <heejongahn@gmail.com>
2020-05-31 16:13:05 +0900

    Implement token renewal logic using refresh_token

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,4 @@ const SHARED_PREFERENCE_LOGIN_EMAIL = 'login_email';
 const SHARED_PREFERENCE_VERSION_KEY = 'version';
 
 const AUTH_LOGIN_TOKEN_KEY = 'loginToken';
+const AUTH_REFRESH_TOKEN_KEY = 'refreshToken';
--- lib/remotes/renew.dart
+++ lib/remotes/renew.dart
@@ -5,13 +5,13 @@ import 'package:galpi/models/auth_token_pair.dart' show AuthTokenPair;
 import 'package:galpi/utils/http_client.dart';
 import 'package:galpi/utils/env.dart';
 
-Future<AuthTokenPair> registerWithFirebase({String firebaseToken}) async {
-  final url = '${env.apiEndpoint}/register/firebase';
+Future<AuthTokenPair> renew({String refreshToken}) async {
+  final url = '${env.apiEndpoint}/auth/renew';
   final response = await httpClient.post(
     url,
     headers: {"Content-Type": "application/json"},
     body: json.encode(
-      {"token": firebaseToken},
+      {"refreshToken": refreshToken},
     ),
   );
 
