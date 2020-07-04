83560bc24acac8ad66709db6f880db541a62f74a
Heejong Ahn <heejongahn@gmail.com>
2020-07-04 12:04:04 +0900

    Show webpage title for webview screen AppBar, if args.title is missing

--- lib/screens/webview/index.dart
+++ lib/screens/webview/index.dart
@@ -9,17 +9,47 @@ class WebviewArgument {
   WebviewArgument(this.title, this.link);
 }
 
-class Webview extends StatelessWidget {
+class Webview extends StatefulWidget {
   final WebviewArgument args;
 
   const Webview({Key key, this.args}) : super(key: key);
 
   @override
+  _WebviewState createState() => _WebviewState();
+}
+
+class _WebviewState extends State<Webview> {
+  WebViewController _controller;
+  String _title = '';
+
+  @override
   Widget build(BuildContext context) {
+    final link = widget.args.link;
+    final linkWithProtocol = link.startsWith('http://')
+        ? link
+        : link.startsWith('https://') ? link : 'https://${link}';
+
     return Scaffold(
-      appBar: AppBar(title: Text(args.title)),
+      appBar: AppBar(
+        title: Text(
+          widget.args.title != '' ? widget.args.title : _title,
+        ),
+      ),
       body: WebView(
-        initialUrl: args.link,
+        onWebViewCreated: (WebViewController controller) async {
+          _controller = controller;
+        },
+        onPageFinished: (_) async {
+          if (_controller == null) {
+            return;
+          }
+
+          final title = await _controller.getTitle();
+          setState(() {
+            _title = title;
+          });
+        },
+        initialUrl: linkWithProtocol,
         javascriptMode: JavascriptMode.unrestricted,
       ),
     );
